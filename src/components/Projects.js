18cec6b98c089f7a8bed01adf961477a0e85e279
craftzcode <tomymaritano@gmail.com>
2024-03-19 19:38:02 +0100

    ups

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,7 +38,7 @@ const Projects = () => {
       })
       .catch((error) => console.error("Error fetching projects:", error));
   }, []);
-  const [showForks] = useState(false);
+  const [showForks, setShowForks] = useState(false);
 
   // const handleShowForksChange = () => {
   //   setShowForks(!showForks);
